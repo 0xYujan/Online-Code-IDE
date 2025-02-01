4758a9da019af6f130997598c10b37e195c31430
Yujan Ranjitkar <63731234+0xYujan@users.noreply.github.com>
2025-02-01 11:35:12 +0545

    Update socket.js

--- frontend/src/socket.js
+++ frontend/src/socket.js
@@ -4,7 +4,7 @@ let socket;
 
 export const initSocket = async () => {
   if (!socket) {
-    socket = io('http://localhost:5000', {
+    socket = io('https://online-code-ide-backend-jvwc.onrender.com', {
       transports: ['websocket'],
       reconnection: true,
       reconnectionAttempts: Infinity,
